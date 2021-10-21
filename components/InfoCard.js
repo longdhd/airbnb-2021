7cf5f7d1c7cc86e7e20793e1c21c3130956e11c9
LongDang <long.danghoangd@gmail.com>
2021-10-21 22:21:50 +0700

    day2_build

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import { HeartIcon } from "@heroicons/react/outline";
 
-function InfoCard({img, destination, title, description, star, price, total}) {
-    return (
-        <div>
-            
-        </div>
-    )
+function InfoCard({
+  img,
+  destination,
+  title,
+  description,
+  star,
+  price,
+  total,
+}) {
+  return (
+    <div>
+      <div className="relative h-24 w-40 md:w-52 md:h-80 flex-shrink-0">
+        <Image src={img} layout="fill" objectFit="cover" />
+      </div>
+      <div className="flex flex-col flex-grow pl-5">
+          <div className="flex justify-between">
+            <p>{location}</p>
+            <HeartIcon className="h-7 cursor-pointer"/>
+          </div>
+      </div>
+    </div>
+  );
 }
 
-export default InfoCard
+export default InfoCard;
